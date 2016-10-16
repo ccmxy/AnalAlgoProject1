e54505de4c3a0804e8dcffa94a47493f2eeb3fd6
ccmxy <windrainicefire@gmail.com>
2016-10-16 16:49:35 -0700

    More additional

--- arrayGenerator.py
+++ arrayGenerator.py
@@ -8,7 +8,6 @@ def generateArrays(numbersPerArray, numberOfArrays, min, max):
             print(random.randrange(min, max)),
         print("]")
 
-generateArrays(1, 10, -200, 200)
 generateArrays(10, 10, -200, 200)
 generateArrays(20, 10, -200, 200)
 generateArrays(50, 10, -200, 200)
@@ -17,9 +16,4 @@ generateArrays(200, 10, -200, 200)
 generateArrays(300, 10, -200, 200)
 generateArrays(400, 10, -200, 200)
 generateArrays(500, 10, -200, 200)
-generateArrays(600, 10, -200, 200)
-generateArrays(700, 10, -200, 200)
-generateArrays(800, 10, -200, 200)
-generateArrays(900, 10, -200, 200)
 generateArrays(1000, 10, -200, 200)
-generateArrays(10000, 10, -200, 200)
